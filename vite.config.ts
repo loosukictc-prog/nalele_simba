8bcfbd274703b170c986abf2f48956ca0c434fd1
Your Name <you@example.com>
2025-11-13 12:03:40 +0300

    chore: configure gh-pages deployment (homepage, base path, deploy directory)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/nalele_simba/',
   server: {
     host: "::",
     port: 8080,
